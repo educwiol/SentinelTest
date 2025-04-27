b48ca3223826d875ab9b1b9f07d33f23e3a8498e
Mr. Gold <hochwilliam6@gmail.com>
2025-04-27 02:40:16 -0400

    MAJOR PUSH PRETTY MUCH WAT SITE IS GONNA LOOK LIKE

--- main/index.js
+++ main/index.js
@@ -7,7 +7,7 @@ function gradient() {
     startTimestamp = +Date.now();
     backgroundDegrees -= ((startTimestamp - lastTimestamp) / (80 + (Math.random() * 20)));
     backgroundDegrees %= 360;
-    document.body.style.background = `linear-gradient(${backgroundDegrees}deg, rgb(3, 0, 14) 0%, rgb(9, 3, 24) 100%)`;
+    document.body.style.background = `linear-gradient(${backgroundDegrees}deg,rgb(255, 255, 255) 0%,rgb(175, 214, 255) 100%)`;
     requestAnimationFrame(gradient);
 }
 
