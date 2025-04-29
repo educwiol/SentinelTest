d8f54e807fdf9369bf21ef6ef161fda0a96b5929
g-ames <95506641+Olijones589@users.noreply.github.com>
2025-04-29 19:36:14 -0400

    change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
 var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
